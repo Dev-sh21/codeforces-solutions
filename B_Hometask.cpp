059c24e35a7bc2ed5758c9eabe3b813a3cf2dfcc
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-01-16 22:50:51 +0530

    Auto commit

---
+++ B_Hometask.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+using ll=long long;
+int main(){
+  ll n;
+  cin>>n;
+  ll sum=0;
+  vector<ll>v(n);
+  for(int i=0;i<n;i++){
+    cin>>v[i];
+    sum+=v[i];
+  }
+  ll remainder=
+  
+  
+  
+}
