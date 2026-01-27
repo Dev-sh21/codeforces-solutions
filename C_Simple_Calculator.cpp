b5be3355137a9055395fcaa35841a46a3ecccbce
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-01-28 01:02:37 +0530

    Auto commit

--- C_Simple_Calculator.cpp
+++ C_Simple_Calculator.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
+using ll=long long;
 int main(){
-  int x,y;
+  ll x,y;
   cin>>x>>y;
   cout<<x<<" + "<<y<<" = "<<x+y<<endl;
-  cout<<x<<" * "<<y<<" = "<<x*y<<endl;
+  cout<<x<<" * "<<y<<" = "<< ll(x*y)<<endl;
   cout<<x<<" - "<<y<<" = "<<x-y<<endl;
   return 0;
 }
