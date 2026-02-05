babcaa5815fddc991f8cf60ebfeed73c48263207
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-02-05 15:07:33 +0530

    Auto commit

---
+++ U_Some_Sums.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int digitSum(int n){
+    int sum=0;
+    while(n>0){
+      sum+=n%10;
+      n/=10;
+    }
+    return sum;
+  }
+
+int main(){
+  int n,a,b;
+  cin>>n>>a>>b;
+  long long total=0;
+  for(int i=1;i<=n;i++){
+    int digit_ka_sum=digitSum(i);
+    if(digit_ka_sum>=a && digit_ka_sum<=b){
+      total+=i;
+    }
+  }
+  cout<<total<<endl;
+  
+
+
+}
