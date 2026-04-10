b5d6b00d2adc59e841a12967bfce52f4d686c76c
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-04-11 01:38:09 +0530

    Auto commit

---
+++ A_Min_Or_Sum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    int sum=0;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+      cin>>arr[i];
+      sum=sum|arr[i];
+
+
+    }
+    cout<<sum<<endl;
+    
+  }
+  return 0;
+
+}
