89e50c42d21046b2e9d25c8a633a40ffadb5c8e2
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-04-11 01:40:32 +0530

    Auto commit

--- A_Regular_Bracket_Sequences.cpp
+++ A_Regular_Bracket_Sequences.cpp
@@ -1,1 +1,34 @@
-#include<
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "(";
+        }
+        for (int j = 0; j < i; j++) {
+            cout << ")";
+        }
+        for (int j = 0; j < n - i; j++) {
+            cout << "()";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
