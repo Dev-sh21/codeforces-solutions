7bea8ee124fbfc493c101e87bf92bdb2dcf9857f
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-04-29 19:53:17 +0530

    new code

---
+++ B_BAN_BAN.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void solve(){
+  int n;
+  cin>>n;
+  int m=(n+1)/2;
+  cout<<m<<endl;
+  int left=2;
+  int right=3*n;
+  while(left<right){
+    cout<<left<<" "<< right<<endl;
+    left+=3;
+    right-=3;
+  }
+
+
+}
+int main(){
+  ios_base::sync_with_stdio(NULL);
+  cin.tie(NULL);
+  int t;
+  cin>>t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
