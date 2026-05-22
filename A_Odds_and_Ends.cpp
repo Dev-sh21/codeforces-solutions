5e2cf8410cb8f14e84b4db4072efd05b15e0752c
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2026-05-22 20:23:03 +0530

    Auto commit

--- A_Odds_and_Ends.cpp
+++ A_Odds_and_Ends.cpp
@@ -1,22 +1,29 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-void solve(){
-  int n;
-  cin>>n;
-  vector<int>arr(n);
-  for(int i=0;i<n;i++){
-    cin>>arr[i];
-  }
-  if(n%2!=0 && arr[0]%2!=0 && arr[n-1]%2!=0) cout<<"Yes\n";
-  else cout<<"N0\n";
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    // Condition 1: Total length odd honi chahiye
+    // Condition 2: Pehla element odd hona chahiye
+    // Condition 3: Aakhri element odd hona chahiye
+    if (n % 2 != 0 && a[0] % 2 != 0 && a[n - 1] % 2 != 0) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
 }
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
 
-  solve();
-  return 0;
-
-
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
 }
